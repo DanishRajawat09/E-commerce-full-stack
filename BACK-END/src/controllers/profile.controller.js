ae66cd62b2ba4b8bed8f3da9dcb118a48a767254
danishRajawat <ibnfarooq070@gmail.com>
2025-06-18 19:12:51 +0530

    create update Profile Fullname

--- BACK-END/src/controllers/profile.controller.js
+++ BACK-END/src/controllers/profile.controller.js
@@ -1,46 +1,80 @@
 import asyncHandler from "../utils/asyncHandler.js";
-import ApiError from "../utils/apiError.js"
+import ApiError from "../utils/apiError.js";
 import uploadImage from "../utils/cloudinary.js";
 import { Profile } from "../models/profile.models.js";
-import ApiResponse from "../utils/apiResponse.js"
-const createProfile = asyncHandler(async (req ,res) => {
-    const {fullName} = req.body
-const user = req.user
-    if (!fullName) {
-        throw new ApiError(400 , "fullname is required")
-    }
-    if (!user) {
-        throw new ApiError(400 , "User not found invalid token")
-    }
+import ApiResponse from "../utils/apiResponse.js";
+const createProfile = asyncHandler(async (req, res) => {
+  const { fullName } = req.body;
+  const user = req.user;
+  if (!fullName) {
+    throw new ApiError(400, "fullname is required");
+  }
+  if (!user) {
+    throw new ApiError(400, "User not found invalid token");
+  }
 
-    const filePath = req.file?.path
+  const filePath = req.file?.path;
 
-    if (!filePath) {
-        throw new ApiError(400 , "avatar not found upload again")
-    }
-const result = await uploadImage(filePath)
-    
-if (!result) {
-    throw new ApiError(400 , "avatar not upload properly")
-}
+  if (!filePath) {
+    throw new ApiError(400, "avatar not found upload again");
+  }
+  const result = await uploadImage(filePath);
 
-const profile = await Profile.create({
+  if (!result) {
+    throw new ApiError(400, "avatar not upload properly");
+  }
+
+  const profile = await Profile.create({
     fullName,
-    avatar : {
-        url : result.url,
-        publicId : result.public_id
+    avatar: {
+      url: result.url,
+      publicId: result.public_id,
     },
-    user : user._id
-})
+    user: user._id,
+  });
 
-if (!profile) {
-    throw new ApiError(500 , "we could not create profile. something went wrong try again")
-}
+  if (!profile) {
+    throw new ApiError(
+      500,
+      "we could not create profile. something went wrong try again"
+    );
+  }
 
-res.status(200).json(
-    new ApiResponse(200 , "profile created succssfully" , profile)
-)
+  res
+    .status(200)
+    .json(new ApiResponse(200, "profile created succssfully", profile));
+});
 
-})
+const updateFullName = asyncHandler(async (req, res) => {
+  const { fullName } = req.body;
+  const user = req.user;
 
-export {createProfile}
+  if (!fullName) {
+    throw new ApiError(400, "fullname is required");
+  }
+  if (!user) {
+    throw new ApiError(400, "Unauthorized user, login again");
+  }
+
+  const exsistingProfile = await Profile.findOne({ user: user._id });
+
+  if (!exsistingProfile) {
+    throw new ApiError(400, "Profile already exsist. you can update only");
+  }
+
+  const profile = await Profile.findOneAndUpdate(
+    { user: user._id },
+    { fullName },
+    { new: true }
+  );
+
+  if (!profile) {
+    throw new ApiError(400, "Profile not found create an profile first");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, "fullname updated successfully", profile));
+});
+
+export { createProfile, updateFullName };
--- BACK-END/src/routes/profile.routes.js
+++ BACK-END/src/routes/profile.routes.js
@@ -1,11 +1,13 @@
-import Router from "express"
-import { createProfile } from "../controllers/profile.controller.js"
-import {upload} from "../middlewares/multer.middleware.js"
-import verifyJwt from "../middlewares/verifyJwt.middleware.js"
-const router = Router()
+import Router from "express";
+import {
+  createProfile,
+  updateFullName,
+} from "../controllers/profile.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import verifyJwt from "../middlewares/verifyJwt.middleware.js";
+const router = Router();
 
-router.route("/create").post(verifyJwt , upload.single("avatar") , createProfile)
+router.route("/create").post(verifyJwt, upload.single("avatar"), createProfile);
+router.route("/update-fullname").post(verifyJwt, updateFullName);
 
-
-
-export default router
+export default router;
