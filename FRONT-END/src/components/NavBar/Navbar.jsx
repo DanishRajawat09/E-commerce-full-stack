94aa5adf887d1b770f0884f2666adb53dcf21e16
danishRajawat <ibnfarooq070@gmail.com>
2025-08-26 23:18:05 +0530

    fonts added in frontend

--- FRONT-END/src/components/NavBar/Navbar.jsx
+++ FRONT-END/src/components/NavBar/Navbar.jsx
@@ -1,60 +1,75 @@
-import React, { useState } from 'react'
-import "./navbar.css"
-import Headline from './Headline'
-import SideNavBar from './SideNavBar'
-import { isOpen } from '../../features/stateSlice'
-import { useDispatch,  } from 'react-redux'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from "react";
+import "./navbar.css";
+import Headline from "./Headline";
+import SideNavBar from "./SideNavBar";
+import { isOpen } from "../../features/stateSlice";
+import { useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const handleSideNav = () => {
+    dispatch(isOpen({ open: true }));
+  };
 
-    const handleSideNav =  () => {
-            dispatch(isOpen({open : true}))
-    }
-
-    return (
-        <header className='navHeader'>
-            
-            <div className='container  '>
-                <div className="flexContainer navData">
-                    <div className='navLogo flexContainer'>
-                        <img className='menuBar' src="burger.png" alt="menuBar" onClick={handleSideNav} />
-                        <h1 className='icon'>shop.co</h1>
-                    </div>
-                    <nav className="navigation">
-
-                        <ul className='flexContainer'>
-                            <li>
-                                <NavLink to={"/"} className={({isActive})=>`shop ${isActive && `navTextColor`}`}>Home</NavLink>
-                                </li>
-                                <li>
-                                <NavLink to={'/shop'} className={({isActive})=>`shop ${isActive && `navTextColor`}`}>Products</NavLink>
-                                </li>
-                               
-                               
-                           
-                        </ul>
-
-                    </nav>
-                    <div className="searchBar">
-                        <div className='userInput flexContainer'>
-                            <img src="Search.png" alt="searchIcon" />
-                            <input type="text" className='searchInput' placeholder='Search for Products...' />
-
-                        </div>
-                    </div>
-                    <div className="cartIdIcon">
-                        <img className='searchIcon' src="search2.png" alt="searchIcon" />
-                     <NavLink to={"/cart"}>
-                     <img src="cart.png" alt="CartIcon" />
-                     </NavLink>
-                        <img src="id.png" alt="IdIcon" />
-                    </div>
-                </div>
+  return (
+    <header className="navHeader">
+      <div className="container  ">
+        <div className="flexContainer navData">
+          <div className="navLogo flexContainer">
+            <img
+              className="menuBar"
+              src="burger.png"
+              alt="menuBar"
+              onClick={handleSideNav}
+            />
+            <h1 className="icon">shop.co</h1>
+          </div>
+          <nav className="navigation">
+            <ul className="flexContainer">
+              <li>
+                <NavLink
+                  to={"/"}
+                  className={({ isActive }) =>
+                    `shop ${isActive && `navTextColor`}`
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/shop"}
+                  className={({ isActive }) =>
+                    `shop ${isActive && `navTextColor`}`
+                  }
+                >
+                  Products
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          <div className="searchBar">
+            <div className="userInput flexContainer">
+              <img src="Search.png" alt="searchIcon" />
+              <input
+                type="text"
+                className="searchInput"
+                placeholder="Search for Products..."
+              />
             </div>
-        </header>
-    )
-}
+          </div>
+          <div className="cartIdIcon">
+            <img className="searchIcon" src="search2.png" alt="searchIcon" />
+            <NavLink to={"/cart"}>
+              <img src="cart.png" alt="CartIcon" />
+            </NavLink>
+            <img src="id.png" alt="IdIcon" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
